8bbac465d319dd2f67e8c4eba30cfe077d4a9756
ramsankarAloor <ramsankar.aloor@gmail.com>
2023-08-05 17:40:53 +0530

    understanding to make axios calls

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+//AXIOS GLOBALS
+axios.defaults.headers.common['X-Auth-Token'] = 
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+
 // GET REQUEST
 function getTodos() {
   // axios({
@@ -49,22 +54,65 @@ function removeTodo() {
 
 // SIMULTANEOUS DATA
 function getData() {
-  console.log('Simultaneous Request');
+  axios.all([
+    axios.get('http://jsonplaceholder.typicode.com/posts'),
+    axios.get('http://jsonplaceholder.typicode.com/todos')
+  ])
+  .then(axios.spread((posts, todos)=> showOutput(posts)))
+  .catch(err=> console.error(err))
 }
 
 // CUSTOM HEADERS
 function customHeaders() {
-  console.log('Custom Headers');
+  const config = {
+    headers : {
+      'Content-Type' : 'application/json',
+      Authorization : 'sometoken'
+    }
+  }
+
+  axios
+  .post('http://jsonplaceholder.typicode.com/todos', {
+    title : 'New todo',
+    completed : false
+  }, config)
+  .then(res => showOutput(res))
+  .catch(err=> console.error(err))
+
 }
 
 // TRANSFORMING REQUESTS & RESPONSES
 function transformResponse() {
-  console.log('Transform Response');
+  const options = {
+    method : 'post',
+    url : 'http://jsonplaceholder.typicode.com/todos',
+    data : {
+      title : 'Hello world'
+    },
+    transformResponse: axios.defaults.transformResponse.concat(data=>{
+      data.title = data.title.toUpperCase()
+      return data;
+    })
+  }
+
+  axios(options).then(res=>showOutput(res))
 }
 
 // ERROR HANDLING
 function errorHandling() {
-  console.log('Error Handling');
+  axios.get('http://jsonplaceholder.typicode.com/todos').then(res=>showOutput(res))
+  .catch(err=>{
+    if(err.response){
+      //server responded with other than 200 range status
+      console.log(err.response.data)
+      console.log(err.response.status)
+      console.log(err.response.headers)
+
+      if(err.response.status===404){
+        alert('Error: Page not found.')
+      }
+    }
+  })
 }
 
 // CANCEL TOKEN
@@ -73,7 +121,13 @@ function cancelToken() {
 }
 
 // INTERCEPTING REQUESTS & RESPONSES
-
+axios.intercetors.request.use(config => {
+  console.log(`${config.method.toUpperCase()} request sent to ${config.url} at ${new Date().getTime()}`)
+  return config
+}, 
+error=>{
+  return Promise.reject(error)
+})
 // AXIOS INSTANCES
 
 // Show output in browser
